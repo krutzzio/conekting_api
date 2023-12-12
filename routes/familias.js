fb71abb0dd1217e62f89199fe6a60cc4b9dd4dd6
Krutzzio <gerard.costa.curr@gmail.com>
2023-12-12 22:40:21 +0100

    saddasf

--- routes/familias.js
+++ routes/familias.js
@@ -3,8 +3,12 @@ const app = express();
 
 const postFamilias = require('../src/familias/postFamilias')
 const getFamilias = require('../src/familias/getFamilias');
+const getFamilia = require('../src/familias/getFamilia')
+
 
 app.route("/api/familias").post(postFamilias);
 app.route("/api/familias").get(getFamilias);
+app.route("/api/familia/:id").get(getFamilia);
+
 
 module.exports = app
--- src/familias/getFamilias.js
+++ src/familias/getFamilias.js
@@ -1,7 +1,12 @@
 const { connection } = require("../../config.db");
 
+const call = `SELECT * FROM familias
+        INNER JOIN familiasusuarios ON familias.idfamilia = familiasusuarios.idfamilia
+        INNER JOIN usuarios ON familiasusuarios.idusuario = usuarios.idusuario
+        WHERE familiasusuarios.organizadorFamilia = 1`
+
 const getFamilias = (request, response) => {
-    connection.query("SELECT * FROM familias",
+    connection.query(call,
         (error, results) => {
             error
                 ? response.status(500).json({ "error": error })
