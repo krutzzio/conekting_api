3f093e274b56250d88d57b342174ee5c8aa367b5
richardulloa <richardjulloa1@gmail.com>
2023-12-13 19:05:48 +0100

    ten piedad api

--- routes/eventos.js
+++ routes/eventos.js
@@ -5,14 +5,14 @@ const getEventos = require('../src/eventos/getEventos')
 const getEvento = require('../src/eventos/getEvento')
 const getMiembrosEvento = require('../src/eventos/getMiembrosEvento')
 const getEventosFamilia = require('../src/eventos/getEventosFamilia')
-/*const postEvento = require('../src/eventos/postEvento') */
+const postEvento = require('../src/eventos/postEvento')
 
 
 app.route("/api/eventos").get(getEventos);
 app.route("/api/eventos/:id").get(getEvento);
 app.route("/api/miembrosevento/:id").get(getMiembrosEvento);
 app.route("/api/eventosfamilia/:id").get(getEventosFamilia);
-/*app.route("/api/eventos").post(postEvento);*/
+app.route("/api/eventos").post(postEvento);
 
 
 module.exports = app
--- src/eventos/postEvento.js
+++ src/eventos/postEvento.js
@@ -1,4 +1,19 @@
 const {connection} = require("../../config.db");
 
-const call = `INSERT INTO usuarios (nombreEvento, descripcionEvento, fechaEvento, calleEvento, numerocalleEvento, cpEvento)  
+const call = `INSERT INTO eventos (nombreEvento, descripcionEvento, fechaEvento, calleEvento, numerocalleEvento, cpEvento)  
 VALUES (?,?,?,?,?,?)`
+
+const postEvento = (request, response) => {
+    const {nombreEvento,descripcionEvento,fechaEvento,calleEvento,numerocalleEvento, cpEvento} = request.body;
+    
+    connection.query(call,[nombreEvento, descripcionEvento, fechaEvento,calleEvento, numerocalleEvento,cpEvento ],
+        (error, results) => {
+            error
+            ?
+            response.status(500).json({"error": error})
+            :
+            response.status(201).json({"Registro Completo!":
+            results.affectedRows});   
+        }
+    )}
+    module.exports = postEvento;
