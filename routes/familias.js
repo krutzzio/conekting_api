abc3de1be6c8f528c0ed96e802348f37563ad9cf
Krutzzio <39598943+Krutzzio@users.noreply.github.com>
2023-12-13 01:24:12 +0100

    cambios

--- routes/familias.js
+++ routes/familias.js
@@ -4,11 +4,12 @@ const app = express();
 const postFamilias = require('../src/familias/postFamilias')
 const getFamilias = require('../src/familias/getFamilias');
 const getFamilia = require('../src/familias/getFamilia')
-
+const getMiembrosFamilia = require('../src/familias/getMiembrosFamilia')
 
 app.route("/api/familias").post(postFamilias);
 app.route("/api/familias").get(getFamilias);
 app.route("/api/familia/:id").get(getFamilia);
+app.route("/api/miembrosfamilia/:id").get(getMiembrosFamilia);
 
 
 module.exports = app
