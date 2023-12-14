51844303c77ed9d863106f56c1cc98cde43ae718
richardulloa <richardjulloa1@gmail.com>
2023-12-14 19:40:42 +0100

    funciona espero no pete

--- src/eventos/postEvento.js
+++ src/eventos/postEvento.js
@@ -1,19 +1,29 @@
-const {connection} = require("../../config.db");
+const { connection } = require("../../config.db");
 
-const call = `INSERT INTO eventos (nombreEvento, descripcionEvento, fechaEvento, calleEvento, numerocalleEvento, cpEvento)  
-VALUES (?,?,?,?,?,?)`
+const call1 = `INSERT INTO eventos (nombreEvento, descripcionEvento, fechaEvento, calleEvento, numerocalleEvento, cpEvento,idfamilia)  
+VALUES (?,?,?,?,?,?,?)`
+
+const call2 = `INSERT INTO eventosusuarios (idevento,idusuario,organizadorEvento) 
+VALUES (?,?,?)`
 
 const postEvento = (request, response) => {
-    const {nombreEvento,descripcionEvento,fechaEvento,calleEvento,numerocalleEvento, cpEvento} = request.body;
-    
-    connection.query(call,[nombreEvento, descripcionEvento, fechaEvento,calleEvento, numerocalleEvento,cpEvento ],
+    const { nombreEvento, descripcionEvento, fechaEvento, calleEvento, numerocalleEvento, cpEvento, idfamilia, idusuario } = request.body;
+
+    connection.query(call1, [nombreEvento, descripcionEvento, fechaEvento, calleEvento, numerocalleEvento, cpEvento, idfamilia],
         (error, results) => {
+            const idevento = results.insertId
             error
-            ?
-            response.status(500).json({"error": error})
-            :
-            response.status(201).json({"Registro Completo!":
-            results.affectedRows});   
+                ?
+                response.status(500).json({ "error": error })
+                :
+                connection.query(call2, [idevento, idusuario, 1],
+                    (error, results) => {
+                        error
+                            ? response.status(500).json({ "error": error })
+                            : response.status(201).json({ "Evento Creado!": results.affectedRows })
+                    }
+                )
         }
-    )}
-    module.exports = postEvento;
+    )
+}
+module.exports = postEvento;
