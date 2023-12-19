52db3c1d3335fcb27a5229e650e986b9a6a34dad
Krutzzio <39598943+Krutzzio@users.noreply.github.com>
2023-12-19 09:49:21 +0100

    safasfgfasgf

--- src/eventos/getEventosInteresUsuario.js
+++ src/eventos/getEventosInteresUsuario.js
@@ -1,29 +1,47 @@
 const { connection } = require("../../config.db");
 
-const call = `select eventos.*, intereses.* from eventos
-    inner join intereseseventos on eventos.idevento = intereseseventos.idevento
-    inner join intereses on intereseseventos.idinteres = intereses.idinteres
-    where intereses.nombreInteres = ?;`
+const call = `SELECT eventos.*, intereses.* FROM eventos
+    INNER JOIN intereseseventos ON eventos.idevento = intereseseventos.idevento
+    INNER JOIN intereses ON intereseseventos.idinteres = intereses.idinteres
+    WHERE `
+
+    /*
+SELECT eventos.*, GROUP_CONCAT(intereses.idinteres) AS intereses_asociados
+FROM eventos
+INNER JOIN intereseseventos ON eventos.idevento = intereseseventos.idevento
+INNER JOIN intereses ON intereseseventos.idinteres = intereses.idinteres
+WHERE intereses.idinteres IN (23, 52, 56, 67, 33, 43, 13, 55, 26, 61, 38, 12, 17, 29)
+GROUP BY eventos.idevento;
+*/
 
 const getEventosInteresUsuario = (request, response) => {
 
     const intereses = request.headers.intereses
 
+    let newIntereses = intereses.split(",")
     let sqlCondition = ""
-    console.log(intereses.split("},{"), typeof(intereses))
+    newIntereses.forEach((interes, index) => {
+        if (index === 0) {
+            return (
+                sqlCondition += `intereses.idinteres = ${interes} `
+            )
+        } else {
+            return (
+                sqlCondition += `OR intereses.idinteres = ${interes} `
+            )
+        }
 
-    intereses.forEach(interes => {
-        return(
-            sqlCondition += `AND intereses.nombreInteres = ${interes.idinteres}`
-        )
     })
-    console.log(intereses, sqlCondition)
-    
-    connection.query(call, [nombre], (error, results) => {
+    let callAdded = call
+    callAdded += sqlCondition
+
+    console.log(callAdded)
+    connection.query(callAdded, (error, results) => {
+        callAdded = call
         error
             ? response.status(500).json({ "error": error })
             : response.status(200).json(results);
     })
-} 
+}
 
 module.exports = getEventosInteresUsuario;
