9d7aa67de84bbf59f7e2ae916aab4c4e5acc3c15
Krutzzio <gerard.costa.curr@gmail.com>
2023-12-20 00:13:20 +0100

    FINAL COMMIT

--- src/eventos/getEventosInteresUsuario.js
+++ src/eventos/getEventosInteresUsuario.js
@@ -1,41 +1,21 @@
 const { connection } = require("../../config.db");
 
-const call = `SELECT eventos.*, intereses.* FROM eventos
+const call = `SELECT eventos.*, GROUP_CONCAT(intereses.idinteres) AS intereses_asociados
+    FROM eventos
     INNER JOIN intereseseventos ON eventos.idevento = intereseseventos.idevento
     INNER JOIN intereses ON intereseseventos.idinteres = intereses.idinteres
-    WHERE `
+    WHERE intereses.idinteres `
 
-    /*
-SELECT eventos.*, GROUP_CONCAT(intereses.idinteres) AS intereses_asociados
-FROM eventos
-INNER JOIN intereseseventos ON eventos.idevento = intereseseventos.idevento
-INNER JOIN intereses ON intereseseventos.idinteres = intereses.idinteres
-WHERE intereses.idinteres IN (23, 52, 56, 67, 33, 43, 13, 55, 26, 61, 38, 12, 17, 29)
-GROUP BY eventos.idevento;
-*/
 
 const getEventosInteresUsuario = (request, response) => {
 
     const intereses = request.headers.intereses
 
-    let newIntereses = intereses.split(",")
-    let sqlCondition = ""
-    newIntereses.forEach((interes, index) => {
-        if (index === 0) {
-            return (
-                sqlCondition += `intereses.idinteres = ${interes} `
-            )
-        } else {
-            return (
-                sqlCondition += `OR intereses.idinteres = ${interes} `
-            )
-        }
-
-    })
+    let sqlCondition = `IN ( ${intereses}) GROUP BY eventos.idevento;`
+    
     let callAdded = call
     callAdded += sqlCondition
 
-    console.log(callAdded)
     connection.query(callAdded, (error, results) => {
         callAdded = call
         error
